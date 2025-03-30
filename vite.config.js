42661713b0f0767f3aedf63a9de79fc83207fc94
anshif <anshif.pk@zartek.in>
2025-03-30 21:03:44 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    base: "/neanderthal/", // 🔥 Set this to your repo name
+    build: {
+      outDir: "dist" // Make sure the build output is correct
+    }// Change output folder from 'dist' to 'build'
+  }
 })
